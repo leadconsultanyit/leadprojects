b71634e149da8997d2f34f49bee82111f328ab2d
Khelan Mehta <khelan05@gmail.com>
2026-05-15 10:54:01 +0530

    pushing

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import './App.css';
+
+if (import.meta.env.VITE_API_URL) {
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
